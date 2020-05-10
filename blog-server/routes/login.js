d380d822f79d500550797a858b551e6a8c8f646e
Katherine Miao <katmiao99@gmail.com>
2020-05-10 00:49:37 -0700

    adding authentication w/JWT cookie

--- blog-server/routes/login.js
+++ blog-server/routes/login.js
@@ -4,12 +4,20 @@ let mongoConn = require("../mongoConn");
 let bcrypt =  require('bcryptjs');
 let jwt = require('jsonwebtoken');
 
-router.get("/", async function(req, res) 
+const secretKey = "C-UFRaksvPKhx1txJYFcut3QGxsafPmwCY6SCly3G6c"
+
+router.get("/:redirect?", async function(req, res) 
 {
+	let username = req.query.username ? req.query.username : ""
+	let password = req.query.password ? req.query.password : ""
+	let redirect = req.query.redirect ? req.query.redirect : ""
+
+	console.log("--- [get] logging in for username=" + username + " password=" + password + " redirect=" + redirect);
+
 	res.render("login", {
-		"username": req.body.username ? req.body.username : "",
-		"password": req.body.password ? req.body.password : "",
-        "redirect": req.body.redirect ? req.body.redirect : ""
+		"username": username,
+		"password": password,
+        "redirect": redirect
     });
 })
 
@@ -18,9 +26,9 @@ router.post("/", async function(req, res)
 {
 	let username = req.body.username;
 	let password = req.body.password;
-	let redirect = req.body.redirect ? req.body.redirect : ""
+	let redirect = req.body.redirect ? req.body.redirect : "";
 
-	console.log("--- logging in for username=" + username + " password=" + password);
+	console.log("--- [post] logging in for username=" + username + " password=" + password + " redirect=" + redirect);
 
 	mongoConn.getDb(async function (db)
   	{
@@ -42,12 +50,26 @@ router.post("/", async function(req, res)
 		  	if (result)
 		  	{
 		  		console.log("--- authentication successful")
+		  		twoHours = Math.floor(Date.now() / 1000) + (2*60*60)
 
-		    	// TODO - set an authentication session cookie in JSWT
+		    	// set an authentication session cookie in JSWT
+		    	// JWT.SIGN(PAYLOAD, SECRETORPRIVATEKEY, HEADER
+		    	let token = jwt.sign(
+		    		{"exp": twoHours, "usr": username},
+		    		secretKey,
+		    		{algorithm: "HS256"} //, type: "JWT"}
+		    	);
 
+		    	res.cookie(
+		    		"JWT", 
+		    		token, 
+		    		{} //{signed: true, httpOnly: true}
+		    	);
+
+		    	console.log("--- jwt & cookie success")
 
 		    	if (redirect == "") {
-		    		res.status(200).send("200 OK: authentication successful!")
+		    		res.status(200).send("Authentication successful!")
 		    	}
 		    	else {
 		    		res.redirect(redirect)
